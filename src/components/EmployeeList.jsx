5de757ffbd5a94238308595494bdc1885a54f570
Mljungho <mathias.ljungholm@gmail.com>
2021-11-24 12:34:21 +0100

    refactoring according to Mr T's example

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,46 +1,20 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { list, Item } from "semantic-ui-react";
 import EmployeeModal from "./EmployeeModal";
-import { Item } from "semantic-ui-react";
 
-class EmployeeList extends Component {
-  state = {
-    employees: [],
-  };
-
-  componentDidMount() {
-    this.getEmployees();
-  }
-
-  getEmployees = async () => {
-    let employeeData = await axios.get("https://reqres.in/api/users");
-    this.setState({ employees: employeeData.data.data });
-  };
-
-  render() {
-    let employeeList = this.state.employees.map((employee) => {
+const EmployeeList = ({employee}) =>  {
       return (
         <Item key={employee.id} className="employee-item">
           <Item.Image
-            ClassName="avatar"
-            circular
-            size="tiny"
-            alt={employee.first_name}
-            src={employee.avatar}
-          />
+            ClassName="avatar" circular size="tiny" alt={employee.first_name} src={employee.avatar}/>
 
           <Item.Content verticalAlign="middle">
             <Item.Header ClassName="name">
               {employee.first_name} {employee.last_name}
             </Item.Header>
-            <Item.Extra>
-              <EmployeeModal id={employee.id} />
-            </Item.Extra>
           </Item.Content>
         </Item>
       );
-    });
-    return ( <Item.Group id="employee-list">{employeeList}</Item.Group> )
-  }
-}
+    };
+  
 export default EmployeeList;
---
+++ src/components/EmployeeListItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EmployeeListItem = ({ employee }) => {
+    return (
+        <React.Fragment>
+            <item data-cy
+            
+        </React.Fragment>
+    )
+}
+
+export default EmployeeListItem
